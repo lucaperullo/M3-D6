77985491181179d6e4606c9380b3c0ed883bb9e8
lucaperullo <72197904+lucaperullo@users.noreply.github.com>
2021-03-16 14:14:51 +0100

    up

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ async function fetchUsers() {
 }
 
 const searchFilter = () => {
-  let filter = document.querySelector("#search");
+  let filter = document.querySelector("#selector").value;
+  if (filter === "name") {
+    console.log("we re in name selector my friend!");
+  } else if (filter === "username") {
+    console.log("we re in username selector else my friend!");
+  } else if (filter === "email") {
+    console.log("we re in email selector else my friend!");
+  }
+  let query = document.querySelector("#searchbar").value.toLowerCase();
+  console.log(query);
 };
 window.onload = startEngine();
